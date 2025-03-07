0cc743e21f64f6ecfc9c949ff2c3433120a11a53
Sanket Gulhane <153764615+Sanket054@users.noreply.github.com>
2025-03-07 20:01:14 +0530

    push

---
+++ src/assets/data.js
@@ -0,0 +1,5 @@
+export const FoodType = [
+	{ type: "main course", poster: "https://i.ibb.co/gLbx3Gfp/main.png" },
+	{ type: "dessert", poster: "https://i.ibb.co/JFSgmtgs/dessert.png" },
+	{ type: "appetizer", poster: "https://i.ibb.co/HpGLzC6f/appetizer.png" }, { type: "bread", poster: "https://i.ibb.co/Swsj8Xxr/bread.png" }, { type: "soup", poster: "https://i.ibb.co/ZpgVtyjv/soup.png" }, { type: "beverage", poster: "https://i.ibb.co/V00w3H9Z/bevrage.png" }, { type: "sauce", poster: "https://i.ibb.co/cGFx24F/sauce.png" }, { type: "fingerfood", poster: "https://i.ibb.co/ynKnqvhZ/fingerfood.jpg" }, { type: "snack", poster: "https://i.ibb.co/h191GHGq/snacks.png" }, { type: "drink", poster: "https://i.ibb.co/zVbTHDwg/drinks.png" }
+]
